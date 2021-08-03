c7506e9e974b1aa7d910cae156878679c746843a
karimsdaher <63963858+karimsdaher@users.noreply.github.com>
2021-08-03 17:31:46 -0400

    file chanfgess

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+
+// import SetList from "../SetList/SetList";
+// import MovieCardDisplay from "../SetList/SetList";
+// import ListSearch from "../ListSearch/ListSearch";
+// import MainPage from "../MainPage/MainPage";
+import ListModal from "../ListModal/ListModal";
+
+function App() {
+	return (
+		<div className="App">
+			<header>
+				<h1>Quick Flick Picker</h1>
+			</header>
+
+			<main>
+				{/* <MovieDisplay /> */}
+        {/* <MainPage /> */}
+				{/* <SetList/> */}
+        <ListModal />
+			</main>
+
+			<footer></footer>
+		</div>
+	);
+}
+
+export default App;
