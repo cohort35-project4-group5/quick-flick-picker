53567c9cdb781a8832dde48e3ce3427e9d3a55d7
Josh <joshetcarson@gmail.com>
2021-08-04 21:36:24 -0400

    Small fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,20 @@ import ListModal from "../ListModal/ListModal";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    <Router>
-      <div className="App">
-        <header>
-          <h1>Quick Flick Picker</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={MainPage} />
-          <Route path="/list" component={SetList} />
-        </main>
-        <footer></footer>
-      </div>
-    </Router>
-  );
-
+	return (
+		<Router>
+			<div className="App">
+				<header>
+					<h1>Quick Flick Picker</h1>
+				</header>
+				<main>
+					<Route exact path="/" component={MainPage} />
+					<Route path="/list" component={SetList} />
+				</main>
+				<footer></footer>
+			</div>
+		</Router>
+	);
+}
 
 export default App;
