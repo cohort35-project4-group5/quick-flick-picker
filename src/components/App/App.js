fb119e917b8f7a4c74588601c2492355e8b90d09
Sean <seankim0423@gmail.com>
2021-08-06 00:13:47 -0400

    more styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,24 +3,25 @@ import MainPage from "../MainPage/MainPage";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-	return (
-		<Router>
-		<div className="App wrapper">
-			<header>
-			<Link to="/">
-				<h1>QuickFlickPix</h1>
-			</Link>
-			</header>
-			<main>
-			<Route exact path="/" component={MainPage} />
-			<Route path="/list/:listname" component={SetList} />
-			</main>
-			<footer>
-			<a href="https://junocollege.com/">Create at Juno College</a>
-			</footer>
-		</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App wrapper">
+        <header>
+          <Link to="/">
+            <h1>QuickFlickPix</h1>
+          </Link>
+        </header>
 
+        <main>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/list/:listname" component={SetList} />
+        </main>
+
+        <footer>
+          <a href="https://junocollege.com/">Create at Juno College</a>
+        </footer>
+      </div>
+    </Router>
+  );
 }
 export default App;
